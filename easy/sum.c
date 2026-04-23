3dd570223875ee9c5013ad08d3d95eb792dcf1da
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-04-23 21:02:03 +0530

    Add C program to calculate sum of two integers using user input

---
+++ easy/sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    int a,b,c;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &a,&b);
+    c=a+b;
+printf("The value of a+b is = %d ", c );
+    return 0;
+}
