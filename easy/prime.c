8c1efc9e9206e57144f1e8e8594fe9ebab1735ab
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-04-26 00:16:19 +0530

    feat: implement prime number check in C

---
+++ easy/prime.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+int i,n,count=0;
+printf("Enter n : ");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+    if(n%i==0){
+        count++;
+    }
+}
+if(count==2) printf("It is a prime number\n");
+    else printf("It is not a prime number\n");
+return 0;
+}
