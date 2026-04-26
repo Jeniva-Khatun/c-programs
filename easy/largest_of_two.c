27c643b59574b666104a74e792e9b46273812371
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-04-26 23:44:45 +0530

    Implement C program to compare two integers and print the larger value using ternary operator

---
+++ easy/largest_of_two.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+ int a,b;
+printf("Enter a : ");
+scanf("%d", &a);
+printf("Enter b : ");
+scanf("%d", &b);
+a>b?printf("a is largest\n"):printf("b is largest\n");
+
+return 0;
+}
