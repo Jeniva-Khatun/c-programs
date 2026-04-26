7bafe1341d05cc139344f2312caf8d9684f61c9f
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-04-27 00:27:50 +0530

    Added logic for this file

--- easy/reverse_number.c
+++ easy/reverse_number.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main (){
+   int num,reverse=0;
+   printf("Enter a number : ");
+   scanf("%d", &num);
+   while (num !=0){
+       int digit=num%10;
+       reverse=reverse*10+digit;
+       num=num/10;
+   }
+    printf("Reverse number is = %d\n ", reverse);
 
+
+    return 0 ;
+}
