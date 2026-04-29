797a89f4a41f5362bb65dbbf98fab5a8bd02da46
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-04-29 21:27:27 +0530

    Update max_btwn_3_num.c

--- easy/max_btwn_3_num.c
+++ easy/max_btwn_3_num.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d %d %d", &a, &b, &c);
     if (a>b && a>c ){
         printf("a is greater\n");
-    } else if (b>c) {
+    } else if (b>c) && (b>a) {
         printf("b is greater\n");
     } else {
         printf("c is greater\n");
