06f8d3424feb1d5057bee5222e295ef4c18d4630
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-05-16 01:40:20 +0530

    feat: implement optimized bubble sort with comparison and swap counter

---
+++ medium/bubble_sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int size) {
+    int comparisons = 0;
+    int swaps = 0;
+
+    for (int i = 0; i < size - 1; i++) {
+        int swapped = 0;
+
+        for (int j = 0; j < size - i - 1; j++) {
+            comparisons++;
+
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swaps++;
+                swapped = 1;
+            }
+        }
+
+        // Stop early if array is already sorted
+        if (swapped == 0) {
+            break;
+        }
+    }
+
+    printf("\nSorted Array: ");
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nTotal Comparisons: %d", comparisons);
+    printf("\nTotal Swaps: %d\n", swaps);
+}
+
+int main() {
+    int size;
+
+    printf("Enter size: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter elements in the array: ");
+
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, size);
+
+    return 0;
+}
