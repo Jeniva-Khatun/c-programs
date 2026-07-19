2fcb6e4dc50e65f4e1a63df1dd49f1414702c134
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-07-19 23:09:46 +0530

    implementing array deletion from any index

---
+++ easy/array_deletion_1.c
@@ -0,0 +1,37 @@
+//delete from any index
+#include<stdio.h>
+int main (){
+
+    int size;
+    int arr[size];
+    int index ;
+
+printf("Enter size of array : ");
+scanf("%d", &size);
+
+
+printf("enter array elements : ");
+for(int i=0; i<size; i++){
+    scanf("%d", &arr[i]);
+}
+
+printf("enter index to delete the value at that index : ");
+scanf("%d", &index);
+
+if(index<0 || index >=size ){
+   return printf("user have entered invalid index");
+}
+int deletevalue = arr[index];
+
+for(int i = index; i<size-1 ; i++){
+    arr[i]=arr[i+1];
+}
+size--;
+printf("After deleting element : ");
+for(int i = 0 ; i<size ; i++){
+    printf("%d ",arr[i]);
+}
+printf("\n%d deleted from index %d\n", deletevalue ,index);
+printf("\n");
+    return 0;
+}
